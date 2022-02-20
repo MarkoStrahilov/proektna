296884e13a8e86d468e6478b83479ff704197247
Marko Strahilov <strahilovmarko@yahoo.com>
2022-02-21 00:04:09 +0100

    Update weather.js

--- weather.js
+++ weather.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async function(e) {
         e.preventDefault()
         const inputValue = form.elements.searchLocation.value;
         const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=712a3f2932621a75cf87e4e875febe7f`)
-            // select elements
+            // call functions
         const locationName = res.data.name;
         cityName(locationName)
         const cityTemperature = res.data.main.temp;
